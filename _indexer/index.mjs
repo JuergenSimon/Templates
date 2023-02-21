b3c93b456950f4707fc8e310f5974598494d47bb
bkleiner <b.kleiner@str8labs.com>
2023-02-22 00:15:55 +0100

    indexer: format

--- _indexer/index.mjs
+++ _indexer/index.mjs
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import YAML from "yaml";
-import Ajv from "ajv/dist/2020.js"
+import Ajv from "ajv/dist/2020.js";
 
 import indexSchema from "./schema/index.json" assert { type: "json" };
 import optionSchema from "./schema/option.json" assert { type: "json" };
@@ -29,7 +29,7 @@ async function* walk(dir) {
     files.push(d);
   }
 
-  if (files.find(f => f.name == INDEX_FILENAME))  {
+  if (files.find((f) => f.name == INDEX_FILENAME)) {
     yield {
       dir,
       files,
@@ -38,15 +38,21 @@ async function* walk(dir) {
 }
 
 async function compileEntries(dir, name) {
-  const entries = []
+  const entries = [];
 
-  const folder = path.join(dir, name)
+  const folder = path.join(dir, name);
   for await (const f of await fs.promises.opendir(folder)) {
-    if (!f.isFile() || path.extname(f.name) != ".yaml" || !f.name.endsWith(".option.yaml")) {
+    if (
+      !f.isFile() ||
+      path.extname(f.name) != ".yaml" ||
+      !f.name.endsWith(".option.yaml")
+    ) {
       continue;
     }
 
-    const entry = YAML.parse(await fs.promises.readFile(path.join(folder, f.name), 'utf8'));
+    const entry = YAML.parse(
+      await fs.promises.readFile(path.join(folder, f.name), "utf8")
+    );
     if (!optionValid(entry)) {
       console.log(optionValid.errors);
       process.exit(1);
@@ -58,18 +64,20 @@ async function compileEntries(dir, name) {
       desc: entry.desc,
       author: entry.author,
       selector: entry.selector,
-      file: path.join(dir, name, f.name)
-    })
+      file: path.join(dir, name, f.name),
+    });
   }
-  
+
   return entries.sort((a, b) => a.title.localeCompare(b.title));
 }
 
 const entries = [];
-for await (const f of walk('.')) {
-  console.log("processing", f.dir)
+for await (const f of walk(".")) {
+  console.log("processing", f.dir);
 
-  const index = YAML.parse(await fs.promises.readFile(path.join(f.dir, INDEX_FILENAME), 'utf8'));
+  const index = YAML.parse(
+    await fs.promises.readFile(path.join(f.dir, INDEX_FILENAME), "utf8")
+  );
   if (!indexValid(index)) {
     console.log(indexValid.errors);
     process.exit(1);
@@ -77,32 +85,35 @@ for await (const f of walk('.')) {
 
   if (index.options) {
     const options = [];
-    for(const option of index.options) {
+    for (const option of index.options) {
       options.push({
         ...option,
-        entries: await compileEntries(f.dir, option.name)
-      })
+        entries: await compileEntries(f.dir, option.name),
+      });
     }
     index.options = options;
   }
-  
+
   const category = path.dirname(f.dir);
   const entry = {
     ...index,
     category,
-    profile: path.join(f.dir, PROFILE_FILENAME)
+    profile: path.join(f.dir, PROFILE_FILENAME),
   };
 
-  const image = f.files.find(f => f.name.startsWith("image"));
+  const image = f.files.find((f) => f.name.startsWith("image"));
   if (image) {
-    entry.image = path.join(f.dir, image.name)
+    entry.image = path.join(f.dir, image.name);
   }
 
   entries.push(entry);
 }
 
-entries.sort((a,b) => {
-  return a.name.localeCompare(b.name)
-})
+entries.sort((a, b) => {
+  return a.name.localeCompare(b.name);
+});
 
-await fs.promises.writeFile("index.json", JSON.stringify(entries, undefined, 2));
+await fs.promises.writeFile(
+  "index.json",
+  JSON.stringify(entries, undefined, 2)
+);
